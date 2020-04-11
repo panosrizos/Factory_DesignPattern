e040ec9eca295bcfef55d4417049f13fe1fbe34c
panosrizos <58530032+panosrizos@users.noreply.github.com>
2020-04-11 04:39:43 +0300

    Initial Commit

---
+++ FactoryDesignPattern/FactoryMethod.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    static class FactoryMethod
+    {
+
+
+        static Printer GetPrinter(string type)
+        {
+            switch (type)
+            {
+                case "InkJet":
+                    return new InkJetPrinter();
+
+                case "Pdf":
+                    return new PdfPrinter();
+                case "Lazer":
+                    return new LazerPrinter();
+
+                default:
+                    return new InkJetPrinter();
+            }
+        }
+
+        public static void Sample()
+        {
+            Console.Write("What Type do you want? ");
+            var type = Console.ReadLine();
+            var printer = GetPrinter(type.Trim());
+
+            //sandwich.Ingredients.Add(new Mustard());
+
+            Console.WriteLine($"Here, we have  {printer.GetType().Name}");
+            printer.GetPrintType();
+            Console.Read();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FactoryMethod.Sample();
+
+        }
+    }
+
+
+    abstract class Printer
+    {
+
+        public abstract void GetPrintType();
+    }
+
+
+
+
+
+
+    class PdfPrinter : Printer
+    {
+
+
+        public override void GetPrintType()
+        {
+            Console.WriteLine("Prints PDF  ");
+        }
+    }
+
+
+
+
+    class InkJetPrinter : Printer
+    {
+        public override void GetPrintType()
+        {
+            Console.WriteLine("Prints in ink");
+        }
+    }
+
+
+
+    class LazerPrinter : Printer
+    {
+        public override void GetPrintType()
+        {
+            Console.WriteLine("Prints with Lazer");
+        }
+    }
+
+
+
+
+
+}
